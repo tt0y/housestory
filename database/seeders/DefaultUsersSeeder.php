12601ef48d6bef1fc2648d749bbf3a72cfb47398
Anton KC <ajusmith@gmail.com>
2021-01-31 23:32:35 +0300

    Added defaultUsersSeeder

---
+++ database/seeders/DefaultUsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DefaultUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'              => 'admin@housestory.loc',
+            'email'             => 'admin@housestory.loc',
+            'password'          => Hash::make(env('DEFAULT_ADMIN_PASS')),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
